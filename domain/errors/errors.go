5b214337c6e1abacbf2af4eb76840f8b1456469e
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-20 20:42:30 -0300

    refactor(errors): add new error types

--- domain/errors/errors.go
+++ domain/errors/errors.go
@@ -1,5 +1,37 @@
 package errors
 
+import "fmt"
+
+type InvalidRouteErr struct {
+	message string
+}
+
+func (e *InvalidRouteErr) Error() string {
+	return e.message
+}
+
+func NewInvalidRouteErr(cause string) *InvalidAirport {
+	return &InvalidAirport{
+		message: fmt.Sprintf("invalid: %s", cause),
+	}
+}
+
+type InvalidAirport struct {
+	message string
+}
+
+func (e *InvalidAirport) Error() string {
+	return e.message
+}
+
+// NewInvalidAirportErr is a constructor for InvalidAirport. You can specify
+// the cause as a parameter.
+func NewInvalidAirportErr(cause string) *InvalidAirport {
+	return &InvalidAirport{
+		message: fmt.Sprintf("airport: %s", cause),
+	}
+}
+
 // RouteNotFoundErr is an error for when routes are not found when searched.
 type RouteNotFoundErr struct {
 	message string
@@ -25,7 +57,7 @@ func (e *BestRouteNotFoundErr) Error() string {
 	return e.message
 }
 
-// NewBestRouteNotFoundErr is aconstructor for BestRouteNotFoundErr.
+// NewBestRouteNotFoundErr is a constructor for BestRouteNotFoundErr.
 func NewBestRouteNotFoundErr() *BestRouteNotFoundErr {
 	return &BestRouteNotFoundErr{
 		message: "best route not found",
