451e04bf3cb9716b280ac143f77361b6d85587cc
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-20 20:39:18 -0300

    refactor(validationservice): implement IsValidCost function

--- domain/services/validationservice/validationservice.go
+++ domain/services/validationservice/validationservice.go
@@ -5,6 +5,11 @@ import (
 	"regexp"
 )
 
+const (
+	min = 1
+	max = 1000000
+)
+
 // IsValidAirport returns true if an airport name matches the expected format.
 func IsValidAirport(airport string) bool {
 	pattern := `^[A-Z]{3}$`
@@ -14,4 +19,8 @@ func IsValidAirport(airport string) bool {
 	}
 
 	return match
+}
+
+func IsValidCost(cost int) bool {
+	return (cost >= min) && (cost <= max)
 }
