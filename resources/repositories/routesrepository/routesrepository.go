b3d66b410fa52e70a9d146dfe9c18373ec930273
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-20 22:21:28 -0300

    refactor(routesrepository): add implementation for routesrepository

--- resources/repositories/routesrepository/routesrepository.go
+++ resources/repositories/routesrepository/routesrepository.go
@@ -1,11 +1,37 @@
 package routesrepository
 
 import (
-	"errors"
-	"go-bestflight/domain/entities/routes"
+	r "go-bestflight/domain/entities/routes"
+	"go-bestflight/resources/cache"
+	"go-bestflight/resources/database"
+	"go-bestflight/resources/file"
+	"log"
 )
 
-// StoreRoute ...
-func StoreRoute(route routes.Route) error {
-	return errors.New("Could not store route")
+func StoreRoute(route r.Route) error {
+	database.StoreAirport(route.Boarding)
+	database.StoreAirport(route.Destination)
+	database.StoreRoute(route)
+
+	err := file.Write(route)
+	if err != nil {
+		log.Printf("error when writing to file: %v", err)
+		log.Println("removing route from database")
+		database.DeleteRoute(route)
+		return err
+	}
+
+	cache.AddRoute(route)
+
+	return nil
+}
+
+func LoadRoutes(routes []r.Route) {
+	database.StoreRoutes(routes)
+
+	for _, route := range routes {
+		database.StoreAirport(route.Boarding)
+		database.StoreAirport(route.Destination)
+		cache.AddRoute(route)
+	}
 }
