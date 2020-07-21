f8e6a4e8020070f1a44071fb6cf6961b49e599ec
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-21 02:24:12 -0300

    refactor(implement GetAllAirports function):

--- resources/repositories/airportrepository/airportrepository.go
+++ resources/repositories/airportrepository/airportrepository.go
@@ -6,3 +6,8 @@ import "go-bestflight/resources/database"
 func IsRegistered(airport string) bool {
 	return database.GetAirport(airport)
 }
+
+// GetAllAirports returns all stored airports.
+func GetAllAirports() []string {
+	return database.GetAllAirports()
+}
