d2b614f6d9f282628651c5a0f481bdbd815bd437
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-21 16:10:49 -0300

    refactor(server): implement the http server

--- application/web/http/server.go
+++ application/web/http/server.go
@@ -1,16 +1,55 @@
 package http
 
 import (
+	"context"
 	"fmt"
 	"go-bestflight/application/web/http/routes"
+	"log"
+	"net/http"
+	"sync"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Start starts http server.
-func Start(port string) {
-	server := gin.Default()
+var (
+	server *http.Server
+)
 
-	routes.InscribeRoutes(server)
-	server.Run(fmt.Sprintf(":%s", port))
+// Start the http server.
+func Start(port string) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		router := gin.Default()
+		routes.InscribeRoutes(router)
+
+		server = &http.Server{
+			Addr:    fmt.Sprintf(":%s", port),
+			Handler: router,
+		}
+
+		wg.Done()
+
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("could not start the HTTP server: %v", err)
+		}
+
+		fmt.Println(fmt.Sprintf("HTTP server running on port %s...", port))
+	}()
+
+	wg.Wait()
+}
+
+func Stop() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	log.Println("Shuting server down...")
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server failed to shutdown:", err)
+	}
 }
---
+++ application/web/http/server_test.go
@@ -0,0 +1,208 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	r "go-bestflight/domain/entities/routes"
+	"go-bestflight/resources/cache"
+	"go-bestflight/resources/database"
+	"go-bestflight/resources/file"
+	"go-bestflight/resources/repositories/routerepository"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/franela/goblin"
+	"github.com/gin-gonic/gin"
+)
+
+func TestComponents(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	gin.SetMode(gin.TestMode)
+
+	log.Println("##############################################")
+
+	Start("3000")
+	time.Sleep(2 * time.Second)
+
+	g.Describe("Tests for the adding of new routes", func() {
+		g.BeforeEach(func() {
+			filePath := "test.csv"
+
+			file.Reset(filePath)
+			database.Connect()
+			database.Truncate()
+			cache.Connect()
+			cache.Truncate()
+		})
+
+		g.AfterEach(func() {
+			file.Remove()
+		})
+
+		g.It("should return status code 201 when adding a new and valid route", func() {
+			route := r.Route{
+				Boarding:    "GRU",
+				Destination: "CDG",
+				Cost:        75,
+			}
+			jsonBytes, _ := json.Marshal(route)
+
+			resp, _ := http.Post("http://localhost:3000/routes", "application/json", bytes.NewReader(jsonBytes))
+			defer resp.Body.Close()
+
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			g.Assert(resp.StatusCode).Equal(201)
+			g.Assert(jsonBytes).Equal(body)
+
+			g.Assert(routerepository.RouteExists(route.Boarding, route.Destination)).IsTrue()
+		})
+
+		g.It("should return status code 200 when adding existing routes", func() {
+			route := r.Route{
+				Boarding:    "GRU",
+				Destination: "CDG",
+				Cost:        75,
+			}
+			jsonBytes, _ := json.Marshal(route)
+
+			http.Post("http://localhost:3000/routes", "application/json", bytes.NewReader(jsonBytes))
+
+			resp, _ := http.Post("http://localhost:3000/routes", "application/json", bytes.NewReader(jsonBytes))
+
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			g.Assert(resp.StatusCode).Equal(200)
+			g.Assert(jsonBytes).Equal(body)
+		})
+
+		g.It("should return status code 400 for a malformed route", func() {
+			route := r.Route{
+				Boarding:    "GRU",
+				Destination: "DG",
+				Cost:        0,
+			}
+			jsonBytes, _ := json.Marshal(route)
+
+			resp, _ := http.Post("http://localhost:3000/routes", "application/json", bytes.NewReader(jsonBytes))
+
+			g.Assert(resp.StatusCode).Equal(400)
+
+			g.Assert(routerepository.RouteExists(route.Boarding, route.Destination)).IsFalse()
+		})
+	})
+
+	g.Describe("Tests for getting best route", func() {
+		// Tests based on:
+		//   GRU,BRC,10
+		//   BRC,SCL,5
+		//   GRU,CDG,75
+		//   GRU,SCL,20
+		//   GRU,ORL,56
+		//   ORL,CDG,5
+		//   SCL,ORL,20
+
+		routes := []r.Route{
+			{
+				Boarding:    "GRU",
+				Destination: "BRC",
+				Cost:        10,
+			},
+			{
+				Boarding:    "BRC",
+				Destination: "SCL",
+				Cost:        5,
+			},
+			{
+				Boarding:    "GRU",
+				Destination: "CDG",
+				Cost:        75,
+			},
+			{
+				Boarding:    "GRU",
+				Destination: "SCL",
+				Cost:        20,
+			},
+			{
+				Boarding:    "GRU",
+				Destination: "ORL",
+				Cost:        56,
+			},
+			{
+				Boarding:    "ORL",
+				Destination: "CDG",
+				Cost:        5,
+			},
+			{
+				Boarding:    "SCL",
+				Destination: "ORL",
+				Cost:        20,
+			},
+		}
+
+		addRoutes := func() {
+			for _, route := range routes {
+				jsonBytes, _ := json.Marshal(route)
+				http.Post("http://localhost:3000/routes", "application/json", bytes.NewReader(jsonBytes))
+			}
+		}
+
+		g.BeforeEach(func() {
+			filePath := "test.csv"
+
+			file.Reset(filePath)
+			database.Connect()
+			database.Truncate()
+			cache.Connect()
+			cache.Truncate()
+
+			addRoutes()
+		})
+
+		g.AfterEach(func() {
+			file.Remove()
+		})
+
+		g.It("should return status code 200 and best route info json when successfully find a the best route", func() {
+			boarding := "gru"
+			destination := "CDG"
+			expectedBestRoute := r.BestRoute{
+				Route: "GRU - BRC - SCL - ORL - CDG",
+				Cost:  40,
+			}
+			jsonData, _ := json.Marshal(expectedBestRoute)
+
+			resp, _ := http.Get(fmt.Sprintf("http://localhost:3000/routes?board=%s&dest=%s", boarding, destination))
+			defer resp.Body.Close()
+
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			g.Assert(resp.StatusCode).Equal(200)
+			g.Assert(jsonData).Equal(body)
+		})
+
+		g.It("should return status code 400 for malformed airport", func() {
+			malformedBoarding := " R1"
+			destination := "CDG"
+
+			resp, _ := http.Get(fmt.Sprintf("http://localhost:3000/routes?board=%s&dest=%s", malformedBoarding, destination))
+
+			g.Assert(resp.StatusCode).Equal(400)
+		})
+
+		g.It("should return status code 204 when the best route could not be found", func() {
+			boarding := "SCL"
+			destination := "GRU"
+
+			resp, _ := http.Get(fmt.Sprintf("http://localhost:3000/routes?board=%s&dest=%s", boarding, destination))
+
+			g.Assert(resp.StatusCode).Equal(204)
+		})
+	})
+}
