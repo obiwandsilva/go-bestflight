8d716b8c21648828a0b6b3c069691958464a482a
Wanderson Silva <wanderson.olivs@gmail.com>
2020-07-21 03:13:00 -0300

    refactor(bestflight): start application from main package

--- cmd/bestflight/main.go
+++ cmd/bestflight/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"go-bestflight/application"
+	"os"
+)
 
+func main() {
+	filePath := os.Args[1]
+	port := os.Args[2]
+
+	application.Start(filePath, port)
 }
